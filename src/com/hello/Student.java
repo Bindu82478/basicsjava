dc076c57f7358d93f3a13657684df6ea877e2a61
ASEHWH123e <bindu82478@gmail.com>
2026-09-08 11:37:27 +0530

    [updated first javaproject]

---
+++ src/com/hello/Student.java
@@ -0,0 +1,12 @@
+package com.hello;
+
+public class Student {
+//to explain sampel hellow owrld
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome tojava world");
+System.out.println("Welcome tojava world");
+System.out.println("Welcome tojava world");
+	}
+
+}
